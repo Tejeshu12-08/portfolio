b4ffc49cc6e62ee7ea9b1e0bb0a5c331629d8483
Tejesh1208 <utejesh1279@gmail.com>
2025-11-04 23:30:09 +0530

    insert txt and js

---
+++ env.js
@@ -0,0 +1,4 @@
+window._env_ = {
+  SUPABASE_URL: "https://tjrliwefwflgpczbcozc.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqcmxpd2Vmd2ZsZ3BjemJjb3pjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyNzEyNTIsImV4cCI6MjA3Nzg0NzI1Mn0.mvHQHk61d5BB2oWnUfOJrS5B9pRHYl_5L7lyV-XFcxY"
+};
